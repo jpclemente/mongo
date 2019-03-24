469621e5ab3dba65c17e756181eb70f5620b1db7
jpclemente <jpclemente91@gmail.com>
2019-03-24 20:30:56 +0100

    [javi] 24-03-2019

--- src/parser.py
+++ src/parser.py
@@ -2,15 +2,32 @@ from datetime import datetime
 
 import xmltodict
 from lxml import etree
+from pymongo import MongoClient
+
+
+def getDict(oDict, id):
+    pass
+
+def write(result):
+    connection = MongoClient('localhost', 27017)
+    db = connection.prueba
+    col = db.publications
+    [col.insert(item) for item in []]
+    connection.close()
+
 
 result = []
 count = 0
 articles = 0
 inproceedings = 0
 incollection = 0
-doc = etree.iterparse('../data/dblp.xml', html=True, events=["end"], tag=["article", "inproceedings", "incollection"])
+limit = 100000
+doc = etree.iterparse('../data/dblp.xml', html=True, events=["end"])
 
+dict = {}
+id = 0
 for event, elem in doc:
+    id += 1
     start = datetime.now()
     if elem.tag in ["article", "inproceedings", "incollection"]:
         count += 1
@@ -20,13 +37,17 @@ for event, elem in doc:
             inproceedings += 1
         else:
             incollection += 1
-        oDict = xmltodict.parse(etree.tostring(elem), encoding="ISO-8859-1")
         print("total: " + str(count))
         print("articles: " + str(articles))
         print("inproceedings: " + str(inproceedings))
         print("incollection: " + str(incollection))
         print("result: " + str(result.__len__()))
-        result.append(oDict)
+        result.append(getDict(elem, id))
+        if result.__len__() == limit:
+            write(result)
+            result = []
     else:
         continue
     elem.clear()
+
+
