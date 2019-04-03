06573c6cfda521ccb0eaff23902fc7172afd00ec
jpclemente <jpclemente91@gmail.com>
2019-04-03 22:20:07 +0200

    [master] generacion de ids de 10 caracteres

--- csv_parser.py
+++ csv_parser.py
@@ -1,32 +1,32 @@
 from lxml import etree
 import pandas as pd
 
-data_path = "data/"
+data_path = "data/" # Ruta donde están localizados los ficheros de datos.
+
+#Diccionario que iremos completando para despues crear los dataframes
 publication_nodes = {'publication_id:ID': [], 'type:LABEL':[], 'date':[], 'title':[]}
 relationships = {'name':[], 'publication_id:END_ID': []}
-count = 0
-total = 0
+
+# inicializamos el id de las publicaciones.
 publication_id = 0
-author_id = 0
-limit = 500000
+
+# construimos una instancia de la clase iterparse que nos permitirá recorrer el xml elemento a elemento.
 doc = etree.iterparse(
-    data_path + "dblp.xml",
-    tag = ["article", "inproceedings", "incollection"],
-    events = ["end"],
-    load_dtd=True,
+    data_path + "test.xml",
+    tag = ["article", "inproceedings", "incollection"], # Solo estamos interesados en etiquetas de estos tipos-
+    events = ["end"],                                   # Solo necesitamos capturar eventos de finalizacion de etiqueta.
+    load_dtd=True,                                      #
     dtd_validation=True,
     encoding="ISO-8859-1")
 for event, elem in doc:
-    count += 1
-    total += 1
     publication_id +=1
-    publication_nodes['publication_id:ID'].append(publication_id)
+    publication_nodes['publication_id:ID'].append(str(publication_id).rjust(10, '0'))
     publication_nodes['type:LABEL'].append(elem.tag)
     publication_nodes['date'].append(elem.get('mdate'))
     for child in elem.getchildren():
         if child.tag == 'author':
             relationships['name'].append(child.text)
-            relationships['publication_id:END_ID'].append(publication_id)
+            relationships['publication_id:END_ID'].append(str(publication_id).rjust(10, '0'))
         elif child.tag == 'title':
             publication_nodes['title'].append(child.text)
     elem.clear()
@@ -40,7 +40,7 @@ df_relationships = pd.DataFrame(relationships)
 relationships = {}
 df_authors = pd.DataFrame(
     {
-        'author_id:ID': [x for x in range(publication_id+1,publication_id+list(set(df_relationships['name'])).__len__() + 1)]
+        'author_id:ID': [str(x).rjust(10, '0') for x in range(publication_id+1,publication_id+list(set(df_relationships['name'])).__len__() + 1)]
         ,'name': list(set(df_relationships['name']))
     }
 )
